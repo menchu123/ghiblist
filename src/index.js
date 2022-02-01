5f35e68b8072fd5fce4b6e8ea0bc86cb6cbb4466
Menchu MV <mcmvillalobos@gmail.com>
2022-02-01 18:12:40 +0100

    Set up query client provider

--- src/index.js
+++ src/index.js
@@ -3,10 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
+const queryClient = new QueryClient();
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
