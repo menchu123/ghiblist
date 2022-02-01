0eea6a07819d33c87728587448deff2086d29cf1
Menchu MV <mcmvillalobos@gmail.com>
2022-02-01 15:48:43 +0100

    Imported MovieCard into App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import MovieCard from "./components/MovieCard/MovieCard";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <MovieCard />
+    </div>
+  );
 }
 
 export default App;
