be46445b25356ab33f197c8a3d452a956a0489c3
Menchu MV <mcmvillalobos@gmail.com>
2022-02-03 10:48:27 +0100

    Created SortBar component

---
+++ src/components/SortBar/SortBar.js
@@ -0,0 +1,74 @@
+import "./SortBar.scss";
+
+const SortBar = ({ setOrder, order }) => {
+  const onChangeOrder = (event) => {
+    if (event.target.checked) {
+      setOrder(event.target.value);
+    }
+  };
+  return (
+    <section className="sort-bar">
+      <p className="sort-bar__title">Sort by:</p>
+      <div className="sort-bar__buttons">
+        <div className="sort-bar__button">
+          <input
+            type="radio"
+            name="sortby"
+            value="release_date"
+            id="release_date"
+            className="sort-bar__input"
+            checked={order === "release_date"}
+            onChange={onChangeOrder}
+          />
+          <label htmlFor="release_date" className="sort-bar__label">
+            Year
+          </label>
+        </div>
+        <div className="sort-bar__button">
+          <input
+            type="radio"
+            name="sortby"
+            value="title"
+            id="title"
+            className="sort-bar__input"
+            checked={order === "title"}
+            onChange={onChangeOrder}
+          />
+          <label htmlFor="title" className="sort-bar__label">
+            Title
+          </label>
+        </div>
+        <div className="sort-bar__button">
+          <input
+            type="radio"
+            name="sortby"
+            value="rt_score"
+            id="rt_score"
+            className="sort-bar__input"
+            checked={order === "rt_score"}
+            onChange={onChangeOrder}
+          />
+          <label htmlFor="rt_score" className="sort-bar__label">
+            Rating
+          </label>
+        </div>
+        <div className="sort-bar__button">
+          <input
+            type="radio"
+            name="sortby"
+            value="running_time"
+            id="running_time"
+            className="sort-bar__input"
+            checked={order === "running_time"}
+            onChange={onChangeOrder}
+          />
+          <label htmlFor="running_time" className="sort-bar__label">
+            Runtime
+          </label>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default SortBar;
