7a5ded583ba1206020cff798eb949c6c2a5fc511
Menchu MV <mcmvillalobos@gmail.com>
2022-02-07 17:29:11 +0100

    Style adjustments to App

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import MovieList from "./components/MovieList/MovieList";
 
 function App() {
   return (
-    <div className="App">
+    <div className="ghiblist">
+      <h1 className="heading">GHIBLIST</h1>
       <MovieList />
     </div>
   );
