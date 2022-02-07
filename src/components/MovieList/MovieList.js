891aacd84e5084ce3ac1a4b79ad9207720d1659b
Menchu MV <mcmvillalobos@gmail.com>
2022-02-07 17:07:21 +0100

    Added addToFilter function and passed it to Filter

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -10,32 +10,37 @@ const MovieList = () => {
   const [filterOptions, setFilterOptions] = useState({});
   const { status, data } = useMovies(filterOptions);
 
+  const addToFilter = (event, filterType) => {
+    setFilterOptions({ ...filterOptions, [filterType]: event.target.value });
+  };
+
   return (
-    <>
-      <Filter
-        setFilterOptions={setFilterOptions}
-        filterOptions={filterOptions}
-      />
-      <SortBar setOrder={setOrder} order={order} />
-      <ul className="movie-list">
-        {status === "loading" ? (
-          <div>Loading...</div>
-        ) : data.length ? (
-          data
-            .sort(
-              order === "title"
-                ? (a, b) =>
-                    a[order].localeCompare(b[order], "en", {
-                      ignorePunctuation: true,
-                    })
-                : (a, b) => b[order] - a[order]
-            )
-            .map((movie) => <MovieCard movie={movie} key={movie.id} />)
-        ) : (
-          <div>No results</div>
-        )}
-      </ul>
-    </>
+    <section className="movies">
+      <div className="movies__sidebar">
+        <Filter addToFilter={addToFilter} />
+      </div>
+      <div className="movies__container">
+        <SortBar setOrder={setOrder} order={order} />
+        <ul className="movie-list">
+          {status === "loading" ? (
+            <div>Loading...</div>
+          ) : data.length ? (
+            data
+              .sort(
+                order === "title"
+                  ? (a, b) =>
+                      a[order].localeCompare(b[order], "en", {
+                        ignorePunctuation: true,
+                      })
+                  : (a, b) => b[order] - a[order]
+              )
+              .map((movie) => <MovieCard movie={movie} key={movie.id} />)
+          ) : (
+            <div>No results</div>
+          )}
+        </ul>
+      </div>
+    </section>
   );
 };
 
