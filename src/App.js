c9855ef5c3e48fba1790a375c1f6fafdf34d145d
Menchu MV <mcmvillalobos@gmail.com>
2022-02-08 17:53:28 +0100

    Routed Home and Detail pages at App

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.scss";
-import MovieList from "./components/MovieList/MovieList";
+import Detail from "./pages/Detail/Detail";
+import Home from "./pages/Home/Home";
 
 function App() {
   return (
     <div className="ghiblist">
       <h1 className="heading">GHIBLIST</h1>
       <Routes>
-        <Route path="/" element={<MovieList />} />
-        <Route path=":id" element={<MovieList />} />
+        <Route path="/" element={<Home />} />
+        <Route path=":movieId" element={<Detail />} />
       </Routes>
     </div>
   );
