8f18db3b3e947d79f3743e010c7716972384a033
Menchu MV <mcmvillalobos@gmail.com>
2022-02-08 17:58:05 +0100

    Changed import style of useMovies at MovieList

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -1,6 +1,6 @@
 import "./MovieList.scss";
 import { useState } from "react/cjs/react.development";
-import useMovies from "../../hooks/useMovies";
+import { useMovies } from "../../hooks/useMovies";
 import SortBar from "../SortBar/SortBar";
 import MovieCard from "../MovieCard/MovieCard";
 import Filter from "../Filter/Filter";
