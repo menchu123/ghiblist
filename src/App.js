083175fe53b19b3c1a5e1bd9cbf38adaccef1543
Menchu MV <mcmvillalobos@gmail.com>
2022-02-09 18:08:27 +0100

    Linked header title to home

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 import "./App.scss";
 import Detail from "./pages/Detail/Detail";
 import Home from "./pages/Home/Home";
@@ -6,7 +6,9 @@ import Home from "./pages/Home/Home";
 function App() {
   return (
     <div className="ghiblist">
-      <h1 className="heading">GHIBLIST</h1>
+      <h1 className="heading">
+        <Link to="/">GHIBLIST</Link>
+      </h1>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path=":movieId" element={<Detail />} />
