1f39e811fc9b1657df71e37950bec3355141faca
Menchu MV <mcmvillalobos@gmail.com>
2022-02-09 12:30:24 +0100

    Added more data to Detail

--- src/pages/Detail/Detail.js
+++ src/pages/Detail/Detail.js
@@ -10,12 +10,26 @@ const Detail = () => {
       {(status === "loading" || !isFetchedAfterMount) && <div>Loading...</div>}
       {isFetchedAfterMount && status === "success" && (
         <>
-          <h2>{data.title}</h2>
-          <img
-            src={data.movie_banner}
-            alt={`${data.title} banner`}
-            height="300"
-          />
+          <section className="movie-detail">
+            <img
+              className="movie-detail__image"
+              src={data.movie_banner}
+              alt={`${data.title} banner`}
+              height="300"
+            />
+            <section className="movie-detail__info">
+              <div className="movie-detail__data">
+                <p className="movie-detail__year">{data.release_date}</p>
+                <p className="movie-detail__runtime">{data.running_time}min</p>
+                <p className="movie-detail__score">{data.rt_score}%</p>
+              </div>
+            </section>
+            <h2 className="movie-detail__title">{data.title}</h2>
+            <p className="movie-detail__director">
+              {data.director} · {data.producer}
+            </p>
+            <p className="movie-detail__description">{data.description}</p>
+          </section>
         </>
       )}
     </>
