fc4aea3f516383531f26cfeaac1ca1991db5b2a0
Menchu MV <mcmvillalobos@gmail.com>
2022-02-14 10:55:44 +0100

    Refactored detail page to fetch data and pass to component

--- src/components/DetailCard/DetailCard.js
+++ src/components/DetailCard/DetailCard.js
@@ -1,41 +1,28 @@
 import "./DetailCard.scss";
-import { useParams } from "react-router-dom";
-import { useMovie } from "../../hooks/useMovies";
 
-const DetailCard = () => {
-  const { movieId } = useParams();
-  const { data, status, isFetchedAfterMount } = useMovie(movieId);
-
+const DetailCard = ({ movie }) => {
   return (
     <>
-      {status === "error" && <div className="error">Could not find movie</div>}
-      {(status === "loading" || !isFetchedAfterMount) && (
-        <div className="loader">Loading...</div>
-      )}
-      {isFetchedAfterMount && status === "success" && (
-        <>
-          <section className="movie-detail">
-            <img
-              className="movie-detail__image"
-              src={data.movie_banner}
-              alt={`${data.title} banner`}
-              height="300"
-            />
-            <section className="movie-detail__info">
-              <div className="movie-detail__data">
-                <p className="movie-detail__year">{data.release_date}</p>
-                <p className="movie-detail__runtime">{data.running_time}min</p>
-                <p className="movie-detail__score">{data.rt_score}%</p>
-              </div>
-            </section>
-            <h2 className="movie-detail__title">{data.title}</h2>
-            <p className="movie-detail__director">
-              {data.director} · {data.producer}
-            </p>
-            <p className="movie-detail__description">{data.description}</p>
-          </section>
-        </>
-      )}
+      <section className="movie-detail">
+        <img
+          className="movie-detail__image"
+          src={movie.movie_banner}
+          alt={`${movie.title} banner`}
+          height="300"
+        />
+        <section className="movie-detail__info">
+          <div className="movie-detail__data">
+            <p className="movie-detail__year">{movie.release_date}</p>
+            <p className="movie-detail__runtime">{movie.running_time}min</p>
+            <p className="movie-detail__score">{movie.rt_score}%</p>
+          </div>
+        </section>
+        <h2 className="movie-detail__title">{movie.title}</h2>
+        <p className="movie-detail__director">
+          {movie.director} · {movie.producer}
+        </p>
+        <p className="movie-detail__description">{movie.description}</p>
+      </section>
     </>
   );
 };
